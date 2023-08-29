94f03c4bdc5b9830ab89896d6c8dd6e613aa1bde
annalaravind <annaldasaravind64@gmail.com>
2023-08-29 20:55:51 +0530

    script updated

---
+++ script.js
@@ -0,0 +1,27 @@
+// DownloadButton
+document.getElementById("downloadButton").addEventListener("click", function () {
+    var resumeURL = "/Images/AnnaldasAravindResume.pdf";
+
+    var downloadAnchor = document.createElement("a");
+    downloadAnchor.setAttribute("href", resumeURL);
+    downloadAnchor.setAttribute("download", "AnnaldasAravindResume.pdf");
+    downloadAnchor.style.display = "none";
+
+    document.body.appendChild(downloadAnchor);
+    downloadAnchor.click();
+
+    document.body.removeChild(downloadAnchor);
+});
+
+// Setup and start animation!
+document.querySelector(".menu-icon").addEventListener("click", function () {
+    const menuItems = document.querySelector("header nav ul");
+    menuItems.classList.toggle("active");
+});
+
+// Setup and start animation!
+var typed = new Typed("#element", {
+    strings: ["Video Editor", "Front End Developer"],
+    typeSpeed: 50,
+});
+
